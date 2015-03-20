f03dd784132775f24c44b42a9b305ce4f8aa74d2
Vardan Andriasyan <vardan.andriasyan@uzh.ch>
2015-03-20 22:33:01 +0100

    added basic main analysis function

--- analyze/processBTCStockInput.R
+++ analyze/processBTCStockInput.R
@@ -4,10 +4,10 @@ library(fields)
 library(rjson)
 
 
-read_file <- "D:\\github\\demo\\input_feeds\\sixify_bitstamp_btcusd.csv"
+read_file <- "C:\\Users\\Vardan\\Documents\\GitHub\\sixify\\analyze\\input_feeds\\sixify_bitstamp_btcusd.csv"
 
-save_file_mean <- "D:\\github\\sixify\\visualize\\sixify_mean_btcusd"
-save_file_sd <- "D:\\github\\sixify\\visualize\\sixify_sd_btcusd_sd"
+save_file_mean <- "..\\visualize\\sixify_mean_btcusd"
+save_file_sd <- "..\\visualize\\sixify_sd_btcusd"
 
 data_table = read.table(read_file, sep=",", header=T)
 
@@ -44,17 +44,3 @@ sink()
 sink(paste(save_file_sd, "txt", sep = ".", collapse = NULL))
 cat(toJSON(binned_data_sd))
 sink()
-
-#p <- ggplot(plot_data, aes(x = data_table$date, y = data_table$price, group = plot_data$id))
-# add points and lines
-#p <- p + geom_point(aes(x = plot_data$time_point, y = plot_data$intensity, colour = plot_data$particle_number), size = I(4), alpha = I(0.4)) + scale_colour_gradient(low="black", high = "magenta")
-#p <- p + geom_line(aes(x = plot_data$time_point, y = plot_data$intensity, colour = plot_data$particle_number), size = I(2), alpha = I(0.4)) 
-# remove background and grid
-#p <- p + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank())
-#p <- p + theme(axis.line = element_line(colour = "black", size = 1))
-# change font size
-#p <- p + theme(text = element_text(size=20, face = "bold"))
-# set y axis limits and ticks
-#p <- p  + scale_y_continuous(limits = c(0, 0.30), breaks= c(0.00, 0.10, 0.20, 0.30)) #  + coord_cartesian(ylim = c(0, 1))
-#ggsave(file=save_file, plot=p, width=10, height=6)
-#print(p);
