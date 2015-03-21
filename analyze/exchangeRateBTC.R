4d1faab0d422384a6cd571117bfe337f4b7677fb
Vardan Andriasyan <vardan.andriasyan@uzh.ch>
2015-03-21 13:19:02 +0100

    added exhchangerate calc

--- analyze/exchangeRateBTC.R
+++ analyze/exchangeRateBTC.R
@@ -10,18 +10,22 @@ read_folder <- "C://Users//Vardan//Documents//GitHub//sixify//analyze//"
 
 #read_file <- "D:\\github\\demo\\input_feeds\\sixify_bitstamp_btcusd.csv"
 
-save_file_geomean <- file.path(read_folder,"..","visualize","data","sixify_bitstamp_geomean_btcusd")
+save_file_geomean <- file.path(read_folder,"..","visualize","data","sixify_geomean_btcusd")
 
 inputPath = file.path(read_folder,"input_feeds","exchanges")
 
 fileList = list.files(path = inputPath,pattern = ".csv$")
 
-
-for iFile in fileList
+# 
+aggrTable =  data.frame(amount=NULL,date =NULL,price = NULL)
+for (i in range(1,length(fileList)))
 { 
-  
-data_table <- read.table(file.path(read_folder,"input_feeds","exchanges",fileList[3]), sep=",", header=T)
-aggrTable = data_table
+#   
+data_table <- read.table(file.path(read_folder,"input_feeds","exchanges",fileList[i]), sep=",", header=T)
+
+
+aggrTable <- rbind(aggrTable, data_table)
+
 }
 
 
@@ -33,31 +37,32 @@ aggrTable = data_table
 # 
 # 
 # 
-# time_bin <- 5*60 # 5 min
+time_bin <- 5*60 # 5 min
+
+#new_timestamps <- seq(min(data_table$date),max(data_table$date)-time_bin,time_bin)
+
+
+bin_boundaries <- seq(min(data_table$date)+time_bin,max(data_table$date)-time_bin,time_bin)
+
+
+A = tapply(data_table$price, cut(data_table$date, breaks = bin_boundaries), geometric.mean)
+
+geomean_price <- as.numeric(A[(1:length(tapply(data_table$price, cut(data_table$date, breaks = bin_boundaries), geometric.mean)))])
+geomean_amount <- as.numeric(A[(1:length(tapply(data_table$price, cut(data_table$date, breaks = bin_boundaries), geometric.mean)))])
+
+
 # 
-# #new_timestamps <- seq(min(data_table$date),max(data_table$date)-time_bin,time_bin)
+binned_data_geomean <- data.frame(price = geomean_price, amount = geomean_amount, date = bin_boundaries[1:length(bin_boundaries)-1])
+
+
+# save csv
 # 
+# write.table(binned_data_geomean, file = paste(save_file_geomean, "csv", sep = ".", collapse = NULL), append=F, row.names=F, col.names=F,  sep=",")
 # 
-# bin_boundaries <- seq(min(data_table$date)+time_bin,max(data_table$date)-time_bin,time_bin)
-# 
-# 
-# A = tapply(data_table$price, cut(data_table$date, breaks = bin_boundaries), geometric.mean)
-# 
-# geomean_price <- as.numeric(A[(1:length(tapply(data_table$price, cut(data_table$date, breaks = bin_boundaries), geometric.mean)))])
-# geomean_amount <- as.numeric(A[(1:length(tapply(data_table$price, cut(data_table$date, breaks = bin_boundaries), geometric.mean)))])
-# 
-# 
-# # 
-# binned_data_geomean <- data.frame(price = geomean_price, amount = geomean_amount, date = bin_boundaries[1:length(bin_boundaries)-1])
-# 
-# 
-# # save csv
-# # 
-# # write.table(binned_data_geomean, file = paste(save_file_geomean, "csv", sep = ".", collapse = NULL), append=F, row.names=F, col.names=F,  sep=",")
-# # 
-#  # save json
-#  
-#  
-# # sink(paste(save_file_geomean, "txt", sep = ".", collapse = NULL))
-# # cat(toJSON(binned_data_geomean))
-# # sink()
+ 
+# save json
+ 
+ 
+ sink(paste(save_file_geomean, "txt", sep = ".", collapse = NULL))
+ cat(toJSON(binned_data_geomean))
+ sink()
